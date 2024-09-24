8c58d5e359992197f189786a0d6ef99d578efea1
AAGONZALEZV96 <aa.gonzalezv96@gmail.com>
2024-09-24 17:49:42 -0400

    Servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TabsComponent } from './components/tabs/tabs.component';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
 import { MenuComponent } from './components/menu/menu.component';
-
-
-@NgModule({
-  declarations: [AppComponent, TabsComponent, MenuComponent],
-=======
 import { Modal1Component } from './components/modal1/modal1.component';
 import { DropDownComponent } from './components/drop-down/drop-down.component';
 import { FormLComponent } from './components/form-l/form-l.component';
-import { MenuComponent } from './components/menu/menu.component';
-
 
 
 @NgModule({
