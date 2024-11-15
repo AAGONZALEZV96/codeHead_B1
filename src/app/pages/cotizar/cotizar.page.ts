38aa4b17e2e83f5f22882d2bdb8d8076023fff24
ChrissJR <christian.or.esp@gmail.com>
2024-11-15 17:58:27 -0300

    Se creo comuna y region de chile

--- src/app/pages/cotizar/cotizar.page.ts
+++ src/app/pages/cotizar/cotizar.page.ts
@@ -6,10 +6,89 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cotizar.page.scss'],
 })
 export class CotizarPage implements OnInit {
+  regiones = [
+    {
+      "NombreRegion": "Arica y Parinacota",
+      "comunas": ["Arica", "Camarones", "Putre", "General Lagos"]
+    },
+    {
+      "NombreRegion": "Tarapacá",
+      "comunas": ["Iquique", "Alto Hospicio", "Pozo Almonte", "Camiña", "Colchane", "Huara", "Pica"]
+    },
+    {
+      "NombreRegion": "Antofagasta",
+      "comunas": ["Antofagasta", "Mejillones", "Sierra Gorda", "Taltal", "Calama", "Ollagüe", "San Pedro de Atacama", "Tocopilla", "María Elena"]
+    },
+    {
+      "NombreRegion": "Atacama",
+      "comunas": ["Copiapó", "Caldera", "Tierra Amarilla", "Chañaral", "Diego de Almagro", "Vallenar", "Alto del Carmen", "Freirina", "Huasco"]
+    },
+    {
+      "NombreRegion": "Coquimbo",
+      "comunas": ["La Serena", "Coquimbo", "Andacollo", "La Higuera", "Paiguano", "Vicuña", "Illapel", "Canela", "Los Vilos", "Salamanca", "Ovalle", "Combarbalá", "Monte Patria", "Punitaqui", "Río Hurtado"]
+    },
+    {
+      "NombreRegion": "Valparaíso",
+      "comunas": ["Valparaíso", "Casablanca", "Concón", "Juan Fernández", "Puchuncaví", "Quintero", "Viña del Mar", "Isla de Pascua", "Los Andes", "Calle Larga", "Rinconada", "San Esteban", "La Ligua", "Cabildo", "Papudo", "Petorca", "Zapallar", "Quillota", "Calera", "Hijuelas", "La Cruz", "Nogales", "San Antonio", "Algarrobo", "Cartagena", "El Quisco", "El Tabo", "Santo Domingo", "San Felipe", "Catemu", "Llaillay", "Panquehue", "Putaendo", "Santa María", "Quilpué", "Limache", "Olmué", "Villa Alemana"]
+    },
+    {
+      "NombreRegion": "Región del Libertador Gral. Bernardo O’Higgins",
+      "comunas": ["Rancagua", "Codegua", "Coinco", "Coltauco", "Doñihue", "Graneros", "Las Cabras", "Machalí", "Malloa", "Mostazal", "Olivar", "Peumo", "Pichidegua", "Quinta de Tilcoco", "Rengo", "Requínoa", "San Vicente", "Pichilemu", "La Estrella", "Litueche", "Marchihue", "Navidad", "Paredones", "San Fernando", "Chépica", "Chimbarongo", "Lolol", "Nancagua", "Palmilla", "Peralillo", "Placilla", "Pumanque", "Santa Cruz"]
+    },
+    {
+      "NombreRegion": "Región del Maule",
+      "comunas": ["Talca", "Constitución", "Curepto", "Empedrado", "Maule", "Pelarco", "Pencahue", "Río Claro", "San Clemente", "San Rafael", "Cauquenes", "Chanco", "Pelluhue", "Curicó", "Hualañé", "Licantén", "Molina", "Rauco", "Romeral", "Sagrada Familia", "Teno", "Vichuquén", "Linares", "Colbún", "Longaví", "Parral", "ReVro", "San Javier", "Villa Alegre", "Yerbas Buenas"]
+    },
+    {
+      "NombreRegion": "Región del Biobío",
+      "comunas": ["Concepción", "Coronel", "Chiguayante", "Florida", "Hualqui", "Lota", "Penco", "San Pedro de la Paz", "Santa Juana", "Talcahuano", "Tomé", "Hualpén", "Lebu", "Arauco", "Cañete", "Contulmo", "Curanilahue", "Los Álamos", "Tirúa", "Los Ángeles", "Antuco", "Cabrero", "Laja", "Mulchén", "Nacimiento", "Negrete", "Quilaco", "Quilleco", "San Rosendo", "Santa Bárbara", "Tucapel", "Yumbel", "Alto Biobío", "Chillán", "Bulnes", "Cobquecura", "Coelemu", "Coihueco", "Chillán Viejo", "El Carmen", "Ninhue", "Ñiquén", "Pemuco", "Pinto", "Portezuelo", "Quillón", "Quirihue", "Ránquil", "San Carlos", "San Fabián", "San Ignacio", "San Nicolás", "Treguaco", "Yungay"]
+    },
+    {
+      "NombreRegion": "Región de la Araucanía",
+      "comunas": ["Temuco", "Carahue", "Cunco", "Curarrehue", "Freire", "Galvarino", "Gorbea", "Lautaro", "Loncoche", "Melipeuco", "Nueva Imperial", "Padre las Casas", "Perquenco", "Pitrufquén", "Pucón", "Saavedra", "Teodoro Schmidt", "Toltén", "Vilcún", "Villarrica", "Cholchol", "Angol", "Collipulli", "Curacautín", "Ercilla", "Lonquimay", "Los Sauces", "Lumaco", "Purén", "Renaico", "Traiguén", "Victoria"]
+    },
+    {
+      "NombreRegion": "Región de Los Ríos",
+      "comunas": ["Valdivia", "Corral", "Lanco", "Los Lagos", "Máfil", "Mariquina", "Paillaco", "Panguipulli", "La Unión", "Futrono", "Lago Ranco", "Río Bueno"]
+    },
+    {
+      "NombreRegion": "Región de Los Lagos",
+      "comunas": ["Osorno", "Puerto Octay", "Purranque", "Río Negro", "San Juan de la Costa", "San Pablo", "Puerto Varas", "Puerto Montt", "Castro", "Ancud", "Dalcahue", "Curaco de Vélez", "Quemchi", "Quinchao", "Osorno", "Calbuco", "Maullín", "Puerto Montt"]
+    },
+    {
+      "NombreRegion": "Región Aysén del General Carlos Ibáñez del Campo",
+      "comunas": ["Coyhaique", "Lago Verde", "Aysén", "Chile Chico", "Cochrane", "O'Higgins", "Río Ibáñez", "Tortel"]
+    },
+    {
+      "NombreRegion": "Región de Magallanes y de la Antártica Chilena",
+      "comunas": ["Punta Arenas", "Puerto Natales", "Porvenir", "Primavera", "Cabo de Hornos", "Antártica", "Puerto Williams", "Laguna Blanca", "Río Verde"]
+    },{
+			"NombreRegion": "Región Metropolitana de Santiago",
+			"comunas": ["Cerrillos", "Cerro Navia", "Conchalí", "El Bosque", "Estación Central", "Huechuraba", "Independencia", "La Cisterna", "La Florida", "La Granja", "La Pintana", "La Reina", "Las Condes", "Lo Barnechea", "Lo Espejo", "Lo Prado", "Macul", "Maipú", "Ñuñoa", "Pedro Aguirre Cerda", "Peñalolén", "Providencia", "Pudahuel", "Quilicura", "Quinta Normal", "Recoleta", "Renca", "San Joaquín", "San Miguel", "San Ramón", "Vitacura", "Puente Alto", "Pirque", "San José de Maipo", "Colina", "Lampa", "TilVl", "San Bernardo", "Buin", "Calera de Tango", "Paine", "Melipilla", "Alhué", "Curacaví", "María Pinto", "San Pedro", "Talagante", "El Monte", "Isla de Maipo", "Padre Hurtado", "Peñaflor"]
+	}
+  ];
+//Variables para origen  
+  selectedRegionOrigen: string = '';
+  selectedComunaOrigen: string = '';
+  selectedComunasOrigen: string[] = [];
 
-  constructor() { }
+  // Variables para destino
+  selectedRegionDestino: string = '';
+  selectedComunaDestino: string = '';
+  selectedComunasDestino: string[] = [];
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  getComunas(type: 'origen' | 'destino') {
+    const selectedRegion = type === 'origen' ? this.selectedRegionOrigen : this.selectedRegionDestino;
+    const region = this.regiones.find(r => r.NombreRegion === selectedRegion);
+
+    if (type === 'origen') {
+      this.selectedComunasOrigen = region ? region.comunas : [];
+    } else if (type === 'destino') {
+      this.selectedComunasDestino = region ? region.comunas : [];
+    }
   }
-
 }
